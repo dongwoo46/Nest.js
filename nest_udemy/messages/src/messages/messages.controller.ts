16423bdc9c085887704037b4009d1754d304dc50
dongwoo <siwol406@gmail.com>
2024-03-24 22:16:08 +0900

    nest udemy 기초

---
+++ nest_udemy/messages/src/messages/messages.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './message.service';
+
+@Controller('/messages')
+export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
+
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
+  }
+
+  @Post()
+  createMessages(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
+
+  @Get('/:id')
+  async getMessages(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found');
+    }
+
+    return message;
+  }
+}
