01e17b9f207766779bd871046d914ff982d8f822
dongwoo <siwol406@gmail.com>
2024-04-28 00:45:59 +0900

    mysql 연결

---
+++ nest_offical/offical/src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { User } from './users/entities/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '1234',
+      database: 'test',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+//EntityManager 객체 프로젝트 전체 주입
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
