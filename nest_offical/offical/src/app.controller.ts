75559ee96e27e339027d0485a0e8e9d5388a2c9c
dongwoo <siwol406@gmail.com>
2024-04-28 22:38:42 +0900

    cookie 데코레이터 사용'

--- nest_offical/offical/src/app.controller.ts
+++ nest_offical/offical/src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Res, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response, Request } from 'express';
+import { Cookies } from './decorators/cookie.decorator';
 
 @Controller()
 export class AppController {
@@ -36,23 +37,13 @@ export class AppController {
     }
   }
 
-  @Get('/cookieRequest')
-  testCookieRequest(@Req() request: Request) {
-    console.log(request.cookies); // or "request.cookies['cookieKey']"
-    // or console.log(request.signedCookies);
+  @Get('/get-cookie')
+  getCookie2(@Cookies() cookies: any) {
+    return cookies; // 모든 쿠키를 가져옴
   }
 
-  @Get('/cookieResponse')
-  testCookieResponse(@Res({ passthrough: true }) response: Response) {
-    response.cookie('cookieName', 'cookieValue', {
-      maxAge: 300000,
-      // none, lax, strict 중 none은 쿠키가 항상 전송되도록 허용.
-      sameSite: 'none', // HTTPS 프로토콜을 사용하고 secure 옵션이 설정된 경우에만 사용 가능
-      secure: true, // 쿠키가 HTTPS 프로토콜을 사용하는 경우에만 전송되도록 제한
-      httpOnly: true, // 쿠키에 접근할 수 있는 영역을 HTTP(S) 프로토콜로 제한하여,
-      // 브라우저의 자바스크립트 코드로부터 쿠키에 접근할 수 없게 함
-    });
-    response.cookie('key', 'value');
-    return response.cookie;
+  @Get('/get-specific-cookie')
+  getSpecificCookie(@Cookies('cookieName') cookieValue: string) {
+    return cookieValue; // 특정 쿠키 값만 가져옴
   }
 }
