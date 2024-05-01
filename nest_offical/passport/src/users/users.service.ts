058d4f51a1456e10231f853fcc1174e943b89735
dongwoo <siwol406@gmail.com>
2024-05-01 12:36:13 +0900

    passport 이용 jwt로그인

---
+++ nest_offical/passport/src/users/users.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
+  create(createUserDto: CreateUserDto): Promise<User> {
+    const user = new User();
+
+    user.username = createUserDto.username;
+    user.password = createUserDto.password;
+
+    return this.usersRepository.save(user);
+  }
+
+  async findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  findOneById(id: number): Promise<User> {
+    return this.usersRepository.findOneBy({ userId: id });
+  }
+
+  async remove(id: string): Promise<void> {
+    await this.usersRepository.delete(id);
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  findOneByUsername(username: string): Promise<User | undefined> {
+    return this.usersRepository.findOneBy({ username: username });
+  }
+}
